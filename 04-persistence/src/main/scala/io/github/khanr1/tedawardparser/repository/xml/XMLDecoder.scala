3759eae457049d43926f9fa58fb57ebd965f03e4
khanr1 <raphael.khan@gmail.com>
2025-11-30 21:59:45 +0200

    [refactoring] ValidateParse

--- 04-persistence/src/main/scala/io/github/khanr1/tedawardparser/repository/xml/XMLDecoder.scala
+++ 04-persistence/src/main/scala/io/github/khanr1/tedawardparser/repository/xml/XMLDecoder.scala
@@ -8,8 +8,14 @@ import cats.syntax.all.*
 
 
 trait XMLDecoder[A]:
-  def decode(e: Elem,path: XMLPath): Either[ParserError,A]
+  def decode(e: Elem,path: XMLPath): ValidateParse[A]
 
 
 object XMLDecoder:
   def apply[A](using decoder: XMLDecoder[A]):XMLDecoder[A]= decoder
+
+
+case class ValidateParse[A](
+  value:A,
+  errors: List[ParserError]
+)
